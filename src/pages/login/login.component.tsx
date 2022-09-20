15cd0dc60088e628feb1c194df58a7e57da0502b
Amaaz Ahmad <amaaz.ahmad@tintash.com>
2022-09-21 01:55:30 +0500

    TYPE: styles
    SUBJECT: Responsive settled and login/signup screens made for larger screens
    
    DESCRIPTION: Added a side-bar for login/signup form for larger screens. Container divs added in login and signup pages for CSS implementation.
    
    YET TO BE DONE:
    - Use Tailwind instead of CSS files
    - Adjust the heading size in the left sidebar of login/signup page for medium sized screens. It stacks on different lines for med screens.

--- src/pages/login/login.component.tsx
+++ src/pages/login/login.component.tsx
@@ -6,34 +6,43 @@ import "./login.styles.css"
 function LoginPage() {
 	console.log("LOGIN PAGE RENDERED")
 	return (
-		<div className="loginContainer">
-			<div className="welcomeTextContainer">
-				<h1
-					style={{fontFamily: "DM Serif Display", margin: 0, fontWeight: "300"}}
-				>
-					Welcome
-				</h1>
-				<p
-					style={{
-						fontFamily: "Lexend Deca",
-						color: "rgba(165, 165, 165, 1)",
-						margin: 0,
-					}}
-				>
-					Let's log you in quickly
+		<div className="fullPageContainer">
+			<div className="leftImageContainer">
+				<h1 className="leftText">Login</h1>
+			</div>
+			<div className="loginContainer">
+				<div className="welcomeTextContainer">
+					<h1
+						style={{
+							fontFamily: "DM Serif Display",
+							margin: 0,
+							fontWeight: "300",
+						}}
+					>
+						Welcome
+					</h1>
+					<p
+						style={{
+							fontFamily: "Lexend Deca",
+							color: "rgba(165, 165, 165, 1)",
+							margin: 0,
+						}}
+					>
+						Let's log you in quickly
+					</p>
+				</div>
+				<UserForm isLogin={true} />
+				<p className="bottomText">
+					Don't have an account?
+					<Link
+						style={{color: "rgba(86, 204, 106, 1)", textDecoration: 0}}
+						to="../signup"
+					>
+						{" "}
+						Signup
+					</Link>
 				</p>
 			</div>
-			<UserForm isLogin={true} />
-			<p className="bottomText">
-				Don't have an account?
-				<Link
-					style={{color: "rgba(86, 204, 106, 1)", textDecoration: 0}}
-					to="../signup"
-				>
-					{" "}
-					Signup
-				</Link>
-			</p>
 		</div>
 	)
 }
--- src/pages/signup/signup.component.tsx
+++ src/pages/signup/signup.component.tsx
@@ -5,34 +5,43 @@ import "./signup.styles.css"
 
 function SignupPage() {
 	return (
-		<div className="loginContainer">
-			<div className="welcomeTextContainer">
-				<h1
-					style={{fontFamily: "DM Serif Display", margin: 0, fontWeight: "300"}}
-				>
-					Welcome
-				</h1>
-				<p
-					style={{
-						fontFamily: "Lexend Deca",
-						color: "rgba(165, 165, 165, 1)",
-						margin: 0,
-					}}
-				>
-					Let's sign you up quickly
+		<div className="fullPageContainer">
+			<div className="leftImageContainer">
+				<h1 className="leftText">Sign Up</h1>
+			</div>
+			<div className="loginContainer">
+				<div className="welcomeTextContainer">
+					<h1
+						style={{
+							fontFamily: "DM Serif Display",
+							margin: 0,
+							fontWeight: "300",
+						}}
+					>
+						Welcome
+					</h1>
+					<p
+						style={{
+							fontFamily: "Lexend Deca",
+							color: "rgba(165, 165, 165, 1)",
+							margin: 0,
+						}}
+					>
+						Let's sign you up quickly
+					</p>
+				</div>
+				<UserForm isLogin={false} />
+
+				<p className="bottomText">
+					Already have an account?
+					<Link
+						style={{color: "rgba(86, 204, 106, 1)", textDecoration: 0}}
+						to="../login"
+					>
+						{" Log-in"}
+					</Link>
 				</p>
 			</div>
-			<UserForm isLogin={false} />
-
-			<p className="bottomText">
-				Already have an account?
-				<Link
-					style={{color: "rgba(86, 204, 106, 1)", textDecoration: 0}}
-					to="../login"
-				>
-					{" Log-in"}
-				</Link>
-			</p>
 		</div>
 	)
 }
