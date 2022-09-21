0f25e5f746a3f4899a682d7429ab2a5aa870d243
Amaaz Ahmad <amaaz.ahmad@tintash.com>
2022-09-22 00:36:34 +0500

    TYPE: styling-fix
    SUBJECT: Styling improved after first demo
    
    DESCRIPTION:
    - Showed user not found error instead of incorrect password error. FIXED
    - Increased padding with left side of form: Changed to 8rem from 4rem after 1024px screen width
    - Error font size slightly increased.

--- src/components/userForm/userForm.component.tsx
+++ src/components/userForm/userForm.component.tsx
@@ -6,6 +6,7 @@ import {useForm} from "react-hook-form"
 import {useNavigate} from "react-router-dom"
 
 import "./userForm.css"
+import {useState} from "react"
 
 type UserFormProps = {
 	isLogin: boolean
@@ -15,11 +16,14 @@ const UserForm = (props: UserFormProps) => {
 	const {isLogin} = props
 	const navigate = useNavigate()
 
+	const [errorMessage, setErrorMessage] = useState<string>()
+
 	const {
 		register,
 		handleSubmit,
 		getValues,
-		//formState: {errors},
+		watch,
+		formState: {errors},
 	} = useForm({
 		defaultValues: {
 			name_input: "",
@@ -29,18 +33,41 @@ const UserForm = (props: UserFormProps) => {
 		},
 	})
 
+	console.log(errors)
+	watch("email_input")
+
 	const onSubmitHandler = handleSubmit(async (data) => {
-		const name = data.name_input
+		//const name = data.name_input
+		console.log("CLICKED")
+		setErrorMessage("")
 		const email = data.email_input
 		const password = data.password_input
 		const confirmPassword = data.confirm_password_input
 
-		console.log(name, email, password, confirmPassword)
+		//console.log(name, email, password, confirmPassword)
+		if (confirmPassword)
+			if (password !== confirmPassword) {
+				setErrorMessage("Password and Confirmed Password do not match.")
+
+				return
+			}
 		let resp
 		if (isLogin) resp = await signInUser(email, password)
 		else resp = await createUserAccount(email, password)
 
 		if (!(resp instanceof Error)) navigate("/home")
+		else {
+			console.log(resp.message)
+			if (resp.message.includes("too-many-requests"))
+				setErrorMessage(
+					"Too many failed attempts to login to this account. Try again later."
+				)
+			else if (resp.message.includes("wrong-password"))
+				setErrorMessage("Incorrect password.")
+			else if (resp.message.includes("user-not-found")) {
+				setErrorMessage("User not found")
+			}
+		}
 	})
 
 	return (
@@ -53,29 +80,54 @@ const UserForm = (props: UserFormProps) => {
 						type="text"
 					/>
 				)}
+				<p
+					className={errors.name_input ? "errorDisplayed" : "errorNotDisplayed"}
+				>
+					{errors.name_input?.message || "a"}
+				</p>
 				<input
-					{...register("email_input", {required: "Email is required."})}
+					{...register("email_input", {
+						required: "Email is required.",
+						pattern: {
+							value:
+								/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+							message: "Invalid email",
+						},
+					})}
 					placeholder="Enter your email"
-					type="email"
 				/>
+				<p
+					className={
+						errors.email_input ? "errorDisplayed" : "errorNotDisplayed"
+					}
+				>
+					{errors.email_input?.message || "a"}
+				</p>
 				<input
 					{...register("password_input", {
 						required: "Password is required.",
 						minLength: {
 							value: 6,
-							message: "Password must be atleast 6 characters long!",
+							message: "Password must be atleast 6 characters long",
 						},
 					})}
 					placeholder="Enter your password"
 					type="password"
 				/>
+				<p
+					className={
+						errors.password_input ? "errorDisplayed" : "errorNotDisplayed"
+					}
+				>
+					{errors.password_input?.message || "a"}
+				</p>
 				{!isLogin && (
 					<input
 						{...register("confirm_password_input", {
 							required: "Confirmed Password is required.",
 							minLength: {
 								value: 6,
-								message: "Password must be atleast 6 characters long!",
+								message: "Password must be atleast 6 characters long",
 							},
 							validate: (value) => {
 								const password_value = getValues("password_input")
@@ -86,7 +138,27 @@ const UserForm = (props: UserFormProps) => {
 						type="password"
 					/>
 				)}
-				<button type="submit">{isLogin ? "LOGIN" : "SUBMIT"}</button>
+				<p
+					className={
+						errors.confirm_password_input
+							? "errorDisplayed"
+							: "errorNotDisplayed"
+					}
+				>
+					{errors.confirm_password_input?.message || "a"}
+				</p>
+
+				<button
+					onClick={() => {
+						setErrorMessage("")
+					}}
+					type="submit"
+				>
+					{isLogin ? "LOGIN" : "SUBMIT"}
+				</button>
+				<p className={errorMessage ? "errorDisplayed" : "errorNotDisplayed"}>
+					{errorMessage || "a"}
+				</p>
 			</form>
 			{/* <form onSubmit={onSubmitHandler}>
 				{!isLogin && (
