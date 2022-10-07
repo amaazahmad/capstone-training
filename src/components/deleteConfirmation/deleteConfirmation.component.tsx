bf07ae4a5a8041b92ec4c0dcd9e606d3783cf788
Amaaz Ahmad <amaaz.ahmad@tintash.com>
2022-10-07 12:29:55 +0500

    style: signout popup modal styling fixed

--- src/components/deleteConfirmation/deleteConfirmation.component.tsx
+++ src/components/deleteConfirmation/deleteConfirmation.component.tsx
@@ -28,9 +28,9 @@ const DeleteConfirmation = ({ blogID, setDeletePopup }: DeleteConfirmationProps)
                <img className='w-20' src="/assets/icons/warning.png" alt="" />
                <h1 className={`${theme ? "text-white" : "text-dark-gray-text-color"} text-center mt-2 text-xl md:mt-4 md:text-4xl`}>Are you sure?</h1>
                <p className=' text-center mt-1 text-l md:mt-2 md:text-xl text-secondary-text-color'>This process can not be undone.</p>
-               <div className=' flex justify-center mt-6 flex-row'>
-                    <button onClick={() => { setDeletePopup(false) }} className=' h-14 w-20 md:w-24 m-2  text-white outline-none bg-secondary-text-color rounded-xl text-l md:text-xl' >Cancel</button>
-                    <button onClick={() => { handleDelete(blogID) }} className=' h-14 w-20 md:w-24 m-2 text-white outline-none bg-red-600 rounded-xl text-l md:text-xl'>{loaderVisible ? <Bars visible={true} height="26" width="30" color="white" wrapperStyle={{ justifyContent: 'center' }} /> : "Delete"}</button>
+               <div className=' flex justify-evenly mt-6 flex-row w-full'>
+                    <button onClick={() => { setDeletePopup(false) }} className=' h-14 w-20 md:w-32 m-2  text-white outline-none bg-secondary-text-color rounded-xl text-l md:text-xl' >Cancel</button>
+                    <button onClick={() => { handleDelete(blogID) }} className=' h-14 w-20 md:w-32 m-2 text-white outline-none bg-red-600 rounded-xl text-l md:text-xl'>{loaderVisible ? <Bars visible={true} height="26" width="30" color="white" wrapperStyle={{ justifyContent: 'center' }} /> : "Delete"}</button>
                </div>
           </div>
      )
